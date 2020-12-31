42b2db9c519db5619f10818e978ff0319507b624
Mo <mcresp495@gmail.com>
2020-12-31 18:44:31 +0200

    added some features

--- Downloder.py
+++ Downloder.py
@@ -1,14 +1,31 @@
-import pytube 
-dl = input('What do you want aduio or video (A/V)? ')
-if dl == "V":
-    url = input("Enter the link video:  ")
-    youtube = pytube.YouTube(url)
-    video = youtube.streams.get_highest_resolution()
-    video.download('../Videos')
-elif dl == "A":
-    url = input("Enter the link video: ")
-    youtube = pytube.YouTube(url)
-    audio = youtube.streams.filter(only_audio=True).all()
-    audio[0].download('../Music')
+from pytube import YouTube
 
-print("Thanks for using Crespo Download")
+def download():
+    url = input('Enter the video link: ')
+    ask = input('What do you want Video or Audio (A/V) : ')
+    if ask == 'V':
+        def video():
+            video = YouTube(url)
+            res = input('Choose the quality (720  -  480  -  360):')
+            res2 = f'{res}p'
+            yt = video.streams.filter(res=res2).all()
+            yt[0].download('/home/mo/Videos')
+        video()
+    elif ask == 'A':
+        def audio():
+            audio = YouTube(url)
+            mp3 = audio.streams.filter(only_audio=True).all()
+            mp3[0].download('home/mo/Music')
+        audio()
+    def again():
+        again = input('Do you want to return (yes/no)')
+        if again == 'yes':
+            download()
+        elif again == 'no':
+            print('Thanks \n bye')
+    again()
+
+download()
+
+
+
